3c6ac8ad4e7bff825f0b3adfe7c5143b4a5198a0
Larry Karnowski <larry@hickorywind.org>
2012-09-11 16:13:49 -0400

    working vertical-looking half-circle demo

--- slider.js
+++ slider.js
@@ -7,15 +7,15 @@ $(document).ready(function() {
 		}
 
 		var circleRadius = 100;
-		var circleMax = circleRadius*2;
+		var circleDiameter = circleRadius*2;
 
 		$(this).parent().children('.slider-content').slider({
 			'animate': false, //the animation messes up the illusion - LJK
 			'min': -1, 
-			'max': (circleMax+1),
-			'value' : (circleMax+1),
+			'max': (circleDiameter+1),
+			'value' : (circleDiameter+1),
 			// 'step': 20,
-			'orientation' : 'vertical',
+			'orientation' : 'horizontal',
 
 			'slide': function(e, ui){
 				var percentLeft;
@@ -25,7 +25,7 @@ $(document).ready(function() {
 				var skip = false;
 				
 				//Show default/disabled/out of bounds state
-				if ( ui.value > 0 && ui.value < (circleMax+1) ) { //if in the valid slide rang
+				if ( ui.value > 0 && ui.value < (circleDiameter+1) ) { //if in the valid slide rang
 					$(this).children('.ui-slider-handle').addClass('is-active');
 				}
 				else {
@@ -33,7 +33,7 @@ $(document).ready(function() {
 				}
 				
 				//Calculate slider's path on circle, put it there, by setting background-position
-				if ( ui.value >= 0 && ui.value <= circleMax ) { //if in valid range, these are one inside the min and max
+				if ( ui.value >= 0 && ui.value <= circleDiameter ) { //if in valid range, these are one inside the min and max
 					var X = Math.sqrt((R*R) - (Y*Y)); //X^2 + Y^2 = R^2. Find X.
 					if ( X == 'NaN' ) {
 						percentLeft = 0;
@@ -42,7 +42,7 @@ $(document).ready(function() {
 						percentLeft = X;
 					}
 				}
-				else if ( ui.value == -1 || ui.value == (circleMax+1) ) {
+				else if ( ui.value == -1 || ui.value == (circleDiameter+1) ) {
 					percentLeft = 0;
 					skip = true;
 				}
@@ -52,12 +52,13 @@ $(document).ready(function() {
 				
 				//Move handle
 				if ( percentLeft > 100 ) { percentLeft = 100; } 
-				$(this).children('.ui-slider-handle').css('background-position',percentLeft +'% 100%'); //set css sprite
+				// $(this).children('.ui-slider-handle').css('background-position',percentLeft +'% 100%'); //set css sprite
+				$(this).children('.ui-slider-handle').css('top',(100-percentLeft) +'%'); //set css sprite
 
 				//Figure out and set input value
 				if ( skip == true ) {
 					submitValue = 'fail';
-					$(this).children('.ui-slider-handle').css('background-position',percentLeft +'% 0%'); //reset css sprite					
+					// LJK: wtf?  $(this).children('.ui-slider-handle').css('background-position',percentLeft +'% 0%'); //reset css sprite
 				}
 				else {
 					submitValue = Math.round(ui.value / 2); //Clamp input value to range 0 - 100
